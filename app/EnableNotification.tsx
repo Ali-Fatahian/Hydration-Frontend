f1dfe580c61f919b55e7a12ec8ed2117b47f1598
Ali-Fattahian <ali.fattahian1378@gmail.com>
2025-04-13 17:37:04 +0200

    style: complete visual design for the EnableNotifications Page

---
+++ app/EnableNotification.tsx
@@ -0,0 +1,43 @@
+import { View, Text, Pressable } from "react-native";
+import React from "react";
+import { Link, Stack } from "expo-router";
+import WaterIcon from "@/assets/WaterIcon";
+import { Ionicons } from "@expo/vector-icons";
+
+type Props = {};
+
+const EnableNotification = (props: Props) => {
+  return (
+    <View className="bg-[#1e1f3f] h-full w-full py-[60px] px-2">
+      <Stack.Screen options={{ headerShown: false }} />
+      <View className="flex justify-center w-full flex-row gap-1">
+        <WaterIcon />
+        <Text className="text-[20px] font-bold text-white text-center flex flex-row">
+          HydrationIQ
+        </Text>
+      </View>
+      <Text className="block mt-[60px] text-center text-white font-bold text-[16px]">
+        Enable Smart Notifications
+      </Text>
+      <Ionicons
+        name="notifications"
+        size={60}
+        color={"#7B61FF"}
+        className="mt-10 text-center"
+      />
+      <Text className="text-center mt-10 text-[#C9C9E3] text-[14px]">
+        Get reminders based on your activity, weather, and creatine usage.
+      </Text>
+      <Pressable className="bg-[#816BFF] mt-[40px] rounded-3xl py-3 px-20 w-fit mx-auto hover:bg-[#735cf5] active:bg-[#5943d6] transition-colors">
+        <Text className="text-sm font-bold text-white text-center">
+          Turn On Notifications
+        </Text>
+      </Pressable>
+      <Text className="text-[#AAAAAA] text-[14px] mt-4 text-center hover:underline">
+        <Link href="/+not-found">Skip for now</Link>
+      </Text>
+    </View>
+  );
+};
+
+export default EnableNotification;
