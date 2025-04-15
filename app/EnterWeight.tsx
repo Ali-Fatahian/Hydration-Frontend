0de861182c302d70d5f91e1d8002a5725835890c
Ali-Fattahian <ali.fattahian1378@gmail.com>
2025-04-15 11:17:39 +0200

    feat: create EnterGender and EnterWeight pages

---
+++ app/EnterWeight.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+type Props = {};
+
+const EnterWeight = (props: Props) => {
+  return (
+    <View>
+      <Text>EnterWeight</Text>
+    </View>
+  );
+};
+
+export default EnterWeight;
