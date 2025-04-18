1a9acc2c6da13aa682488dad25ff77731a76cd15
Ali-Fattahian <ali.fattahian1378@gmail.com>
2025-04-18 16:33:11 +0200

    feat: create the PersonalInfomation page

---
+++ app/PersonalInformation.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+type Props = {};
+
+const PersonalInformation = (props: Props) => {
+  return (
+    <View>
+      <Text>PersonalInformation</Text>
+    </View>
+  );
+};
+
+export default PersonalInformation;
