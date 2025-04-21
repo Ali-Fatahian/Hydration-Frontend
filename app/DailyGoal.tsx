f4c0238f6ec474cc81d92ead18dfb04cffc903c2
Ali-Fattahian <ali.fattahian1378@gmail.com>
2025-04-21 11:29:05 +0200

    fix: add a wrapper view to the DailyGoal page to center everything and give it a large maximum width

--- app/DailyGoal.tsx
+++ app/DailyGoal.tsx
@@ -58,70 +58,72 @@ const DailyGoal = (props: Props) => {
   return (
     <View className="bg-[#1e1f3f] h-full w-full py-[60px]">
       <Stack.Screen options={{ headerShown: false }} />
-      <View className="flex justify-center w-full flex-row gap-1">
-        <WaterIcon />
-        <Text className="text-[20px] font-bold text-white text-center flex flex-row">
-          HydrationIQ
+      <View className="w-full max-w-lg mx-auto">
+        <View className="flex justify-center w-full flex-row gap-1">
+          <WaterIcon />
+          <Text className="text-[20px] font-bold text-white text-center flex flex-row">
+            HydrationIQ
+          </Text>
+        </View>
+        <Text className="block mt-[60px] text-center text-white font-bold text-[16px]">
+          Set your daily goal
         </Text>
-      </View>
-      <Text className="block mt-[60px] text-center text-white font-bold text-[16px]">
-        Set your daily goal
-      </Text>
-      <View className="w-full mt-[50] px-2">
-        <TextInput
-          className='w-full max-w-lg mx-auto peer transition-all bg-[#2D2F50] border border-[#3D3F6E] focus:border-none font-light px-5 py-3 text-sm text-white rounded-md outline-none select-all focus:bg-[#373964]"'
-          keyboardType="numeric"
-          placeholder="Enter your daily goal(ml)"
-          value={dailyGoal}
-          onChange={(e: any) => handleInputChange(e.target.value)}
-        />
-        {error && error.length > 0 && (
-          <View className="w-full mt-3">
-            <View className="bg-[#B22222] p-2 rounded-md w-full max-w-lg mx-auto">
-              <Text className="text-sm text-gray-200">{error}</Text>
+        <View className="w-full mt-[50] px-2">
+          <TextInput
+            className='w-full max-w-lg mx-auto peer transition-all bg-[#2D2F50] border border-[#3D3F6E] focus:border-none font-light px-5 py-3 text-sm text-white rounded-md outline-none select-all focus:bg-[#373964]"'
+            keyboardType="numeric"
+            placeholder="Enter your daily goal(ml)"
+            value={dailyGoal}
+            onChange={(e: any) => handleInputChange(e.target.value)}
+          />
+          {error && error.length > 0 && (
+            <View className="w-full mt-3">
+              <View className="bg-[#B22222] p-2 rounded-md w-full max-w-lg mx-auto">
+                <Text className="text-sm text-gray-200">{error}</Text>
+              </View>
+            </View>
+          )}
+          <View className="bg-[#565967] px-5 py-4 rounded-lg mt-4">
+            <View className="">
+              <Ionicons
+                name="bulb"
+                color={"#FBC02D"}
+                size={15}
+                className="mr-2"
+              />
+              <Text className="text-[#afafc1] font-light text-xs mt-3">
+                <Text className="font-bold">How to calculate:</Text> Water
+                intake (in ounces) = Body weight (in pounds) × 0.5
+              </Text>
+              <Text className="text-[#afafc1] font-light text-xs mt-2">
+                For example, If you weigh 150 pounds: 150lbs × 0.5 = 75 ounces
+              </Text>
+              <Text className="text-[#afafc1] font-light text-xs mt-4">
+                <Text className="font-bold">Exercise:</Text> Add 12–16 ounces
+                for every 30 minutes of activity.
+              </Text>
+              <Text className="text-[#afafc1] font-light text-xs mt-4">
+                <Text className="font-bold">Heat:</Text> If you're in a hot
+                climate, aim to drink more to compensate for water lost through
+                sweat.
+              </Text>
             </View>
           </View>
-        )}
-        <View className="bg-[#565967] px-5 py-4 rounded-lg mt-4">
-          <View className="">
-            <Ionicons
-              name="bulb"
-              color={"#FBC02D"}
-              size={15}
-              className="mr-2"
-            />
-            <Text className="text-[#afafc1] font-light text-xs mt-3">
-              <Text className="font-bold">How to calculate:</Text> Water intake
-              (in ounces) = Body weight (in pounds) × 0.5
+          <Pressable
+            onPress={formSubmitHandler}
+            className="bg-[#816BFF] mt-[40px] rounded-3xl py-3 px-20 w-fit mx-auto hover:bg-[#735cf5] active:bg-[#5943d6] transition-colors"
+          >
+            <Text className="text-sm font-bold text-white text-center">
+              Continue
             </Text>
-            <Text className="text-[#afafc1] font-light text-xs mt-2">
-              For example, If you weigh 150 pounds: 150lbs × 0.5 = 75 ounces
-            </Text>
-            <Text className="text-[#afafc1] font-light text-xs mt-4">
-              <Text className="font-bold">Exercise:</Text> Add 12–16 ounces for
-              every 30 minutes of activity.
-            </Text>
-            <Text className="text-[#afafc1] font-light text-xs mt-4">
-              <Text className="font-bold">Heat:</Text> If you're in a hot
-              climate, aim to drink more to compensate for water lost through
-              sweat.
-            </Text>
-          </View>
+          </Pressable>
+          <Link
+            href="/Dashboard"
+            className="text-white text-[14px] mt-3 font-light text-center hover:underline active:underline"
+          >
+            Cancel
+          </Link>
         </View>
-        <Pressable
-          onPress={formSubmitHandler}
-          className="bg-[#816BFF] mt-[40px] rounded-3xl py-3 px-20 w-fit mx-auto hover:bg-[#735cf5] active:bg-[#5943d6] transition-colors"
-        >
-          <Text className="text-sm font-bold text-white text-center">
-            Continue
-          </Text>
-        </Pressable>
-        <Link
-          href="/Dashboard"
-          className="text-white text-[14px] mt-3 font-light text-center hover:underline active:underline"
-        >
-          Cancel
-        </Link>
       </View>
     </View>
   );
