c0b0b6b3095a380c33f162896637ab5454675a73
Ali-Fattahian <ali.fattahian1378@gmail.com>
2025-04-21 12:36:22 +0200

    feat: add the input and modal functionalities to the CreatineIntake page

--- app/CreatineIntake.tsx
+++ app/CreatineIntake.tsx
@@ -1,14 +1,60 @@
-import { View, Text, Modal, Pressable } from "react-native";
-import React, { useState } from "react";
+import { View, Text, Modal, Pressable, TextInput } from "react-native";
+import React, { useEffect, useState } from "react";
 import { Stack, useRouter } from "expo-router";
 import WaterIcon from "@/assets/WaterIcon";
 import BottleIcon from "@/assets/BottleIcon";
+import axios from "axios";
 
 type Props = {};
 
 const CreatineIntake = (props: Props) => {
   const [modalVisible, setModalVisible] = useState(false);
-  const router = useRouter()
+  const [creatineIntake, setCreatineIntake] = useState("");
+  const [backendIntakeVal, setBackendIntakeVal] = useState("");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const sendData = async () => {
+    try {
+      const response = await axios.post("url", {});
+      if (response.status === 200) {
+        setMessage(response.data);
+      }
+    } catch (err: any) {
+      setError(err.message);
+    }
+  };
+
+  const formSubmitHandler = () => {
+    if (creatineIntake && creatineIntake.length > 0) {
+      if (!isNaN(Number(creatineIntake))) {
+        sendData();
+      } else {
+        setError("You must type in numbers.");
+      }
+    } else {
+      setError("The field must not be empty.");
+    }
+  };
+
+  const fetchCreatineIntake = async () => {
+    try {
+      const response = await axios.get("url", {});
+      if (response.status === 200) {
+        setCreatineIntake(response.data);
+        setBackendIntakeVal(response.data);
+      } else {
+        setError("Something went wrong, please try again.");
+      }
+    } catch (err: any) {
+      setError(err.message);
+    }
+  };
+
+  useEffect(() => {
+    // fetchCreatineIntake()
+  }, []);
 
   return (
     <View className="bg-[#1e1f3f] h-full w-full py-[40px] px-2">
@@ -23,15 +69,55 @@ const CreatineIntake = (props: Props) => {
             setModalVisible(false);
           }}
         >
-          <Text className="p-2 pb-0 bg-white">This is a modal!</Text> // This
-          will open an input field for the amount of creatine intake
-          <Pressable
-            onPress={() => setModalVisible(false)}
-            className="cursor-pointer p-2 pt-0 bg-white"
-          >
-            <Text className="hover:underline">Close</Text>
-          </Pressable>
+          <View className="flex-1 justify-center items-center bg-opacity-80 bg-black">
+            <View className="bg-[#2E2E5D] p-6 rounded-lg w-[90%] max-w-lg">
+              <Text className="text-white text-lg font-bold mb-4">
+                Enter Creatine Intake (g/day)
+              </Text>
+
+              <TextInput
+                className="peer transition-all bg-[#2D2F50] border border-[#3D3F6E] focus:border-none font-light px-5 py-3 w-full text-sm text-white rounded-md outline-none select-all focus:bg-[#373964]"
+                placeholder="e.g. 3"
+                placeholderTextColor="#C9C9E3"
+                keyboardType="numeric"
+                onChangeText={(v) => {
+                  setError("");
+                  setCreatineIntake(v);
+                }}
+                value={creatineIntake}
+              />
+              {error.length > 0 && (
+                <View className="bg-[#B22222] mt-3 p-2 rounded-md">
+                  <Text className="text-sm text-gray-200">{error}</Text>
+                </View>
+              )}
+              {message.length > 0 && (
+                <View className="bg-[#3CB371] mt-3 p-2 rounded-md">
+                  <Text className="text-sm text-gray-200">{message}</Text>
+                </View>
+              )}
+              <Pressable
+                onPress={() => {
+                  formSubmitHandler();
+                }}
+                className="mt-4 py-2 hover:bg-[#448AFF] bg-[#2979FF] transition-colors rounded-lg w-full"
+              >
+                <Text className="text-white text-center font-semibold">
+                  Submit
+                </Text>
+              </Pressable>
+              <Pressable
+                onPress={() => setModalVisible(false)}
+                className="mt-4 py-2 bg-gray-600 hover:bg-gray-500 transition-colors rounded-lg w-full"
+              >
+                <Text className="text-white text-center font-semibold">
+                  Close
+                </Text>
+              </Pressable>
+            </View>
+          </View>
         </Modal>
+
         <View className="flex justify-center w-full flex-row gap-1">
           <WaterIcon />
           <Text className="text-[20px] font-bold text-white text-center flex flex-row">
@@ -50,7 +136,12 @@ const CreatineIntake = (props: Props) => {
           className="mt-5 p-3 rounded-lg bg-[#2D2F50] hover:bg-[#373964] transition-colors cursor-pointer"
         >
           <Text className="text-white font-bold text-[14px]">
-            Enter amount (g/day)
+            Enter amount (g/day){" "}
+            {backendIntakeVal && backendIntakeVal.length > 0 && (
+              <Text className="text-xs text-gray-400 font-normal">
+                Currently: {backendIntakeVal}g
+              </Text>
+            )}
           </Text>
           <Text className="text-white text-[12px] font-light mt-2">
             Most users take 3-5g/day
@@ -58,7 +149,7 @@ const CreatineIntake = (props: Props) => {
         </Pressable>
         <View className="bg-[#2E2E5D] px-3 py-5 flex flex-col gap-6 rounded-lg mt-10">
           <View className="flex flex-row justify-between items-center gap-4 w-full max-w-[300px] mx-auto">
-            <BottleIcon height={70} width={70} fill={'white'} />
+            <BottleIcon height={70} width={70} fill={"white"} />
             <View className="flex flex-col gap-2">
               <Text className="text-white text-[14px] font-bold">
                 HydrateFuel Creatine
@@ -74,14 +165,9 @@ const CreatineIntake = (props: Props) => {
             </Text>
           </Pressable>
         </View>
-        <Pressable className="bg-[#816BFF] mt-[40px] rounded-3xl py-3 px-20 w-fit mx-auto hover:bg-[#735cf5] active:bg-[#5943d6] transition-colors">
-          <Text className="text-sm font-bold text-white text-center">
-            Continue
-          </Text>
-        </Pressable>
         <Pressable
-          onPress={() => router.push('/Profile')} // router.back() ||
-          className="text-white text-[14px] mt-3 font-light text-center hover:underline active:underline"
+          onPress={() => router.push("/Profile")} // router.back() ||
+          className="text-white font-bold mt-6 text-center hover:underline active:underline"
         >
           Back
         </Pressable>
